705aa3b8b268635a189ee6adba22e08799b1ef58
David Park <xdavidpark@gmail.com>
2019-06-20 23:42:48 -0700

    Add merge and quicksorts

---
+++ fibs_sum.js
@@ -0,0 +1,9 @@
+// Implement a method that finds the sum of the first n
+// fibonacci numbers recurisvely. Assume n > 0
+
+function fibsSum(n) {
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+
+  return fibsSum(n-1) + fibsSum(n-2) + 1;
+}
